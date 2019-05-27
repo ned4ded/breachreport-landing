9a53e5db2549db5f2117316edb34a476bf404aee
Bazhenov Eugen <wtf.czabe@gmail.com>
2019-05-27 13:35:20 +0300

    ad js counter to stats

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -1,10 +1,44 @@
 $( document ).ready(function () {
-  const formatter = (num) => Math.round(num).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  const formatter = {
+    comma: (num) => Math.round(num).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
+    billion: (n) => {
+      const adjustNumber = (str, n = 1) => {
+        if(str.length >= n) return str
+
+        return adjustNumber('0' + str, n)
+      }
+
+      const num = (function() {
+        const num = Math.round(n).toString()
+
+        return num.length > 3 ? num : adjustNumber(num, 3)
+      })()
+
+      return num.replace(/(\d)(?=(\d{2})+(?!\d))/g, '$1.')
+    },
+
+  }
 
   $('[data-toggle="popover"]').popover()
 
-  $('[data-count-number]').countTo({
-    formatter,
-    refreshInterval: 2
-  });
+  const count = el => {
+    const $el = $( el )
+    const formatterName = $el.data('formatter') || 'comma'
+
+    const { [formatterName]:formatterFn } = formatter
+
+    $el.countTo({
+      formatter: formatterFn,
+      refreshInterval: 2,
+      onComplete: function() {
+        setTimeout(() => {
+          count(this)
+
+          return
+        }, 1000)
+      }
+    })
+  }
+
+  $('[data-count-number]').each((i, e) => count(e))
 })
